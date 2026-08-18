0042575fafc8eea99a7d48bab1d27c3e27dafe48
Seri Leem <129329833+SeriLeem@users.noreply.github.com>
2026-08-18 19:30:42 +0900

    add exercises for dictionary

---
+++ Container/dictionary_exercises.py
@@ -0,0 +1,37 @@
+#ex1 making dictionary with various ways
+# my_dict={}
+# print(type(my_dict))
+# print(my_dict)
+# my_dict_1={'fishing':'to catch a fish', 'fishing boats':'boats used for fishing'}
+# print(my_dict_1)
+
+# my_dict_2={1:'Hydrogen', 2:'Helium', 2:'Lithium', 3:'Lithium'}
+# print(my_dict_2)
+
+# my_dict_3={'name':'mr.Kang', 14003:[3,2,0,1]}
+# print(my_dict_3)
+
+# my_dict_4=dict([(1,'seoul'), (2,'Busan')])
+# print(my_dict_4)
+# my_dict_5={'name':'aaa', 'age':25, 'manager':'Samsung', 'job':'researcher'}
+# print(my_dict_5)
+# price={('noodle', 'rice'):'20 dollars', ('pizza', 'hotdog'):'40dollars'} #using tuples as keys
+# print(price)
+
+#ex2 Dictionary Comprehension
+# d1={w:1 for w in 'abcdef'}
+# print(d1)
+
+# make a dictionary with two lists
+# import sys
+# keys=['one', 'two', 'three']
+# values=[1,2,3]
+# d=dict(zip(keys, values))
+
+# for k in d:
+#     print(k)
+# print()
+
+# for key, value in d.items():
+#     print(key, value)
+
